03e2a18747de797ed1153528b47a50f95d062758
MeryemNairi <meriemnairi01@gmail.com>
2024-05-02 13:18:35 +0100

    final commit

--- src/webparts/backOffice/components/Forme/Forme.tsx
+++ src/webparts/backOffice/components/Forme/Forme.tsx
@@ -5,6 +5,8 @@ import styles from './Forme.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { AiOutlineFile } from 'react-icons/ai';
+
 
 
 export const Forme: React.FC<IFormProps> = ({ context }) => {
@@ -108,30 +110,34 @@ export const Forme: React.FC<IFormProps> = ({ context }) => {
 
   return (
     <div className={styles.backOfficeContainer} style={{ position: 'relative' }}>
-    <svg
-      width="20%" // Largeur à 100% de la largeur du conteneur parent
-      height="20%" // Hauteur à 100% de la hauteur du conteneur parent
-      viewBox="0 0 526 525"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={{
-        top: 0,
-        left: 20,
-        zIndex: -1, // Assurez-vous que le fond SVG est placé en arrière-plan
-      }}
-    >
-      <rect x="350" width="175" height="175" rx="87.5" fill="#00AB84" />
-      <rect y="350" width="175" height="175" rx="87.5" fill="#00AB84" />
-      <rect x="176" y="175" width="175" height="175" rx="87.5" fill="#00AB84" />
-      <path d="M175 0H350V175C253.35 175 175 96.6498 175 0Z" fill="#FEE45E" />
-      <path d="M175 175L175.152 350L0.15168 350.152C0.0679441 253.502 78.3502 175.084 175 175Z" fill="#FEE45E" />
-      <path d="M350 175H525V350C428.35 350 350 271.65 350 175Z" fill="#002920" />
-      <path d="M524.922 524.078L349.922 523.921L350.079 348.921C446.729 349.008 525.009 427.429 524.922 524.078Z" fill="#002920" />
-      <rect width="175" height="175" fill="#002920" />
-      <path d="M351 524L175 349V524H351Z" fill="#FFC46B" />
-    </svg>
-    
-    <h2>Back Office</h2>
+      
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+      
+        <svg
+          width="60%"
+          height="60%"
+          viewBox="0 0 526 525"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ marginRight: '20px' }}
+        >
+          <rect x="350" width="175" height="175" rx="87.5" fill="#00AB84" />
+          <rect y="350" width="175" height="175" rx="87.5" fill="#00AB84" />
+          <rect x="176" y="175" width="175" height="175" rx="87.5" fill="#00AB84" />
+          <path d="M175 0H350V175C253.35 175 175 96.6498 175 0Z" fill="#FEE45E" />
+          <path d="M175 175L175.152 350L0.15168 350.152C0.0679441 253.502 78.3502 175.084 175 175Z" fill="#FEE45E" />
+          <path d="M350 175H525V350C428.35 350 350 271.65 350 175Z" fill="#002920" />
+          <path d="M524.922 524.078L349.922 523.921L350.079 348.921C446.729 349.008 525.009 427.429 524.922 524.078Z" fill="#002920" />
+          <rect width="175" height="175" fill="#002920" />
+          <path d="M351 524L175 349V524H351Z" fill="#FFC46B" />  
+                </svg>
+          <div>
+          <h2 style={{ fontFamily: 'Montserrat', fontSize: '40px', fontWeight: 600, lineHeight: '97.52px', textAlign: 'left', marginBottom: '10px' }}>Gestion des Offres d'Emploi</h2>
+          <p style={{ fontFamily: 'Montserrat', fontSize: '16px' }}>Bienvenue sur notre plateforme de gestion des offres d'emploi. Ici, vous pouvez facilement saisir, modifier et gérer toutes les offres d'emploi que votre entreprise propose.
+
+</p>
+        </div>
+      </div>
       <form className={styles.formContainer} onSubmit={handleSubmit}>
         <div className={styles.inputField}>
           <h3 className={styles.inlineTitle}>Offre Title:</h3>
@@ -139,7 +145,7 @@ export const Forme: React.FC<IFormProps> = ({ context }) => {
         </div>
         <div className={styles.inputField}>
           <h3 className={styles.inlineTitle}>Short Description:</h3>
-          <input type="text" id="short_description" name="short_description"  value={formData.short_description} onChange={handleInputChange} className={styles.ShortDescription} />
+          <input type="text" id="short_description" name="short_description" value={formData.short_description} onChange={handleInputChange} className={styles.ShortDescription} />
         </div>
         <div className={styles.inputField}>
           <h3 className={styles.inlineTitle}>Deadline:</h3>
@@ -194,9 +200,9 @@ export const Forme: React.FC<IFormProps> = ({ context }) => {
               <td>
                 {entry.fileUrl ? (
                   <span
-                    style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }}
+                    style={{ textDecoration: 'underline', color: 'red', cursor: 'pointer' }}
                     onClick={() => window.open(entry.fileUrl, '_blank')}>
-                    fichier
+                    <AiOutlineFile style={{ marginRight: '5px' }} />
                   </span>
                 ) : (
                   '-'
@@ -207,13 +213,13 @@ export const Forme: React.FC<IFormProps> = ({ context }) => {
                 <span className={styles.iconSpace}></span>
                 <FontAwesomeIcon icon={faTrash} onClick={() => handleDeleteEntry(entry.id)} style={{ color: 'black', cursor: 'pointer' }} />
               </td>
-
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
+
 };
 
 export default Forme;
